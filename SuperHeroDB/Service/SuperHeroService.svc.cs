95cad299ac2e1aa0b9f0d4dd2917fddbbfdfe251
newmanuevers <seangnewman@msn.com>
2020-10-05 17:12:09 -0700

    Completed CRUDD functions.  Changed delete from course to function similar to other

---
+++ SuperHeroDB/Service/SuperHeroService.svc.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+
+namespace SuperHeroDB.Service
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class SuperHeroService
+    {
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        public List<SuperHero> GetAllHeroes (){
+            return Data.SuperHeroes;
+        }
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "GetHero/{id}")]
+        
+        public SuperHero GetHero( string id)
+        {
+            
+            return Data.SuperHeroes.Find(sh => sh.Id == int.Parse(id));
+        }
+
+        [OperationContract]
+        [WebInvoke(ResponseFormat =WebMessageFormat.Json, BodyStyle =WebMessageBodyStyle.Bare, UriTemplate ="AddHero", Method ="POST")]
+
+        public SuperHero AddHero(SuperHero hero)
+        {
+            hero.Id = Data.SuperHeroes.Max(sh => sh.Id) + 1;
+            Data.SuperHeroes.Add(hero);
+            return hero;
+        }
+
+
+        [OperationContract]
+        [WebInvoke(ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "UpdateHero/{id}", Method = "PUT")]
+
+        public SuperHero UpdateHero(SuperHero updatedHero, string id)
+        {
+            SuperHero hero = Data.SuperHeroes.Where(sh => sh.Id == int.Parse(id)).FirstOrDefault();
+
+            hero.FirstName = updatedHero.FirstName;
+            hero.LastName = updatedHero.LastName;
+            hero.HeroName = updatedHero.HeroName;
+            hero.PlaceOfBirth = updatedHero.PlaceOfBirth;
+            hero.Combat = updatedHero.Combat;
+
+            return hero;
+        }
+
+
+
+        [OperationContract]
+        [WebInvoke(ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "DeleteHero/{id}", Method = "DELETE")]
+
+        public List<SuperHero> DeleteHero(string id)
+        {
+            SuperHero hero = Data.SuperHeroes.FirstOrDefault(sh => sh.Id == int.Parse(id));
+            Data.SuperHeroes.Remove(hero);
+            return Data.SuperHeroes;
+          }
+
+    }
+}
